685e06c8b4594569b79c7e40fa624ed4db0715ee
poziegbe <poziegbe@opay.team>
2022-08-28 09:50:04 +0100

    Added age calculator service

---
+++ src/main/java/com/demo/filter/RateLimiterFilter.java
@@ -0,0 +1,66 @@
+package com.demo.filter;
+
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RateLimiterFilter implements Filter {
+    private final int MAX_REQUESTS_PER_SECOND = 3;
+    private final LoadingCache<String, Integer> requestCountsPerIpAddress;
+
+    public RateLimiterFilter(){
+        super();
+        requestCountsPerIpAddress = Caffeine.newBuilder().
+                expireAfterWrite(1, TimeUnit.SECONDS).build(new CacheLoader<String, Integer>() {
+                    public Integer load(String key) {
+                        return 0;
+                    }
+                });
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        String clientIpAddress = getClientIP((HttpServletRequest) servletRequest);
+        if(isMaximumRequestsPerSecondExceeded(clientIpAddress)){
+            httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            httpServletResponse.getWriter().write("Too many requests");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isMaximumRequestsPerSecondExceeded(String clientIpAddress){
+        Integer requests = 0;
+        requests = requestCountsPerIpAddress.get(clientIpAddress);
+        if(requests != null){
+            if(requests > MAX_REQUESTS_PER_SECOND) {
+                requestCountsPerIpAddress.asMap().remove(clientIpAddress);
+                requestCountsPerIpAddress.put(clientIpAddress, requests);
+                return true;
+            }
+        } else {
+            requests = 0;
+        }
+        requests++;
+        requestCountsPerIpAddress.put(clientIpAddress, requests);
+        return false;
+    }
+
+    public String getClientIP(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null){
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
+    }
+}
