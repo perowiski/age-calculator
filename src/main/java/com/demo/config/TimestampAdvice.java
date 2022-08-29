72b287cc036a8c0a0b4be2faf0395dd1e89daf7f
perowiski <peterabcxyz@yahoo.com>
2022-08-29 23:48:36 +0100

    Added global exception handler

--- src/main/java/com/demo/config/TimestampAdvice.java
+++ src/main/java/com/demo/config/TimestampAdvice.java
@@ -1,9 +1,10 @@
 package com.demo.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 
 import java.beans.PropertyEditorSupport;
@@ -11,17 +12,19 @@ import java.sql.Timestamp;
 
 @ControllerAdvice
 public class TimestampAdvice {
-    private final Logger logger = LoggerFactory.getLogger(TimestampAdvice.class);
-
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(Timestamp.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) throws IllegalArgumentException {
-                logger.info("Advice Timestamp: {}", text);
                 Timestamp time = Timestamp.valueOf(text);
                 setValue(time);
             }
         });
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<Object> exception(IllegalArgumentException exception) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
